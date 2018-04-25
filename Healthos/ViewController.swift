3f11f78c71a9b51e43175cd60bac7084630d2e7e
Suraj Shinde <suraj.shinde@conduent.com>
2018-04-25 12:07:52 +0530

    Test App.

---
+++ Healthos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Healthos
+//
+//  Created by Suraj Shinde on 4/25/18.
+//  Copyright © 2018 Suraj Shinde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
